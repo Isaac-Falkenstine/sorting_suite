96cee2e365f527ce61dee0ddfc97785145e77b23
Isaac Falkenstine <isaac@falkenstine.com>
2019-01-21 11:31:06 -0700

    Added some logic to the bubble sort method

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,7 +3,11 @@ function bubbleSort(array) {
   let flip;
   do {
     flip = false;
-
+    flip = false;
+    for (let i = 1; i < sa.length; ++i) {
+      if (sa[i - 1] > sa[i]) {
+        [ sa[i], sa[i - 1] ] = [ sa[i - 1], sa[i] ];
+        flip = true;
       }
     }
   } while (flip == true)
